9a436728a02c10af8900cf1a1336e2fc66b3731a
Void7esports <darmumin52@gmail.com>
2026-01-20 01:16:04 +0530

    Update scrims-data.js

--- scrims-data.js
+++ scrims-data.js
@@ -1,9 +1,8 @@
-// DO NOT TOUCH NAME
 const SCRIM_HISTORY = [
-  { date: "13 Jan 2026", standing: 18 },
-  { date: "14 Jan 2026", standing: 12 },
-  { date: "15 Jan 2026", standing: 10 },
-  { date: "16 Jan 2026", standing: 14 },
-  { date: "17 Jan 2026", standing: 8 },
-  { date: "18 Jan 2026", standing: 5 }
+  { date: "13 Jan 2026", standing: 1 },
+  { date: "15 Jan 2026", standing: 16 },
+  { date: "16 Jan 2026", standing: 15 },
+  { date: "17 Jan 2026", standing: 5 },
+  { date: "18 Jan 2026", standing: 10 },
+  { date: "19 Jan 2026", standing: 9 }
 ];
