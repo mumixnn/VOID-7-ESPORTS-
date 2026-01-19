34c4324792a36187a8ca5cac00e219c9ca684bb2
Void7esports <darmumin52@gmail.com>
2026-01-20 01:10:06 +0530

    Create scrims-data.js

---
+++ scrims-data.js
@@ -0,0 +1,9 @@
+// DO NOT TOUCH NAME
+const SCRIM_HISTORY = [
+  { date: "13 Jan 2026", standing: 18 },
+  { date: "14 Jan 2026", standing: 12 },
+  { date: "15 Jan 2026", standing: 10 },
+  { date: "16 Jan 2026", standing: 14 },
+  { date: "17 Jan 2026", standing: 8 },
+  { date: "18 Jan 2026", standing: 5 }
+];
