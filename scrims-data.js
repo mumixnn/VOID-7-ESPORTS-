8c0254436434ad94b3511046fcd68abe6071a438
Void7esports <darmumin52@gmail.com>
2026-01-20 15:03:25 +0530

    Update scrims-data.js

--- scrims-data.js
+++ scrims-data.js
@@ -1,12 +1,14 @@
-scrims.push({
-  date: "2026-01-20",
-  month: "January",
-  year: 2026,
-  players: [
-    { name: "LUFFY", kills: 9, damage: 1860 },
-    { name: "VIEL", kills: 19, damage: 2826 },
-    { name: "GOD", kills: 14, damage: 2408 },
-    { name: "RUNE", kills: 7, damage: 734 },
-    { name: "MYTH", kills: 0, damage: 0 }
-  ]
-});
+const scrims = [
+  {
+    date: "2026-01-19",
+    month: "January",
+    year: 2026,
+    players: [
+      { name: "LUFFY", kills: 9, damage: 1860 },
+      { name: "VIEL", kills: 19, damage: 2826 },
+      { name: "GOD", kills: 14, damage: 2408 },
+      { name: "RUNE", kills: 7, damage: 734 },
+      { name: "MYTH", kills: 0, damage: 0 }
+    ]
+  },
+  {
